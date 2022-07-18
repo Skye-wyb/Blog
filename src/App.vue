88ddb30ad8e32f9e629a7beeda184ff4a7b8ed1d
Yuanyuan Zhao <zhaoyy09@rd.netease.com>
2022-07-18 12:47:23 +0800

    feat:bb

--- src/App.vue
+++ src/App.vue
@@ -17,6 +17,15 @@
       <el-button type="warning" icon="el-icon-star-off" circle></el-button>
       <el-button type="danger" icon="el-icon-delete" circle></el-button>
     </el-row>
+    <hr />
+    <el-row>
+      <el-button>默认按钮</el-button>
+      <el-button type="primary">主要按钮</el-button>
+      <el-button type="success">成功按钮</el-button>
+      <el-button type="info">信息按钮</el-button>
+      <el-button type="warning">警告按钮</el-button>
+      <el-button type="danger">危险按钮</el-button>
+    </el-row>
   </div>
 </template>
 
